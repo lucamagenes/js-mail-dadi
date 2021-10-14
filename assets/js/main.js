53bfd802eb09b901ca68b6348c313bf4e788b510
lucamagenes <lucamgns33@gmail.com>
2021-10-14 15:14:04 +0200

    aggiunto al commit iniziale

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/////////////////MAIL
+
+//Chiedi all’utente la sua email
+
+//controlla che sia nella lista di chi può accedere
+
+//stampa un messaggio appropriato sull’esito del controllo
+
+
+
+
+
+
+
+
+
+/////////////////DADI
+
+//Generare un numero random da 1 a 6,
+
+//per il giocatore
+
+//per il computer
+
+//Stabilire il vincitore
+  //in base a chi fa il punteggio più alto
+
+
