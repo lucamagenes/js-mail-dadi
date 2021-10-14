98f295ea77cd3b10cf8cdd6be03e12ad3ef02be4
lucamagenes <lucamgns33@gmail.com>
2021-10-14 16:58:41 +0200

    dadi
    creato numero casuale giocatore
    creato numero casuale pc
    
    creata condizione di vittoria

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 
 //Chiedi all’utente la sua email
 
+/* 
 let userMail = prompt('Inserisci la tua mail');
 console.log(userMail);
 
@@ -11,9 +12,11 @@ const negato = document.getElementById('negato');
 const abilitato = document.getElementById('abilitato');
 
 
+//controlla che sia nella lista di chi può accedere
 for (let index = 0; index < approvedMails.length; index++) {
     const approvedMail = approvedMails[index];
 
+    //stampa un messaggio appropriato sull’esito del controllo
     if (userMail === approvedMail) {
         console.log('Accesso abilitato');
         abilitato.innerHTML = "Accesso abilitato";
@@ -23,16 +26,7 @@ for (let index = 0; index < approvedMails.length; index++) {
         negato.innerHTML = "Accesso negato";
     }
 }
-
-
-
-
-//controlla che sia nella lista di chi può accedere
-
-//stampa un messaggio appropriato sull’esito del controllo
-
-
-
+ */
 
 
 
@@ -43,9 +37,26 @@ for (let index = 0; index < approvedMails.length; index++) {
 
 //Generare un numero random da 1 a 6,
 
+
 //per il giocatore
+var userPlay = Math.floor((Math.random() * 6) + 1);
+console.log("il tuo numero: ", userPlay);
+
+
 
 //per il computer
+var pcPlay = Math.floor((Math.random() * 6) + 1);
+console.log("il numero del pc: ", pcPlay);
+
+if (userPlay > pcPlay) {
+    console.log("hai vinto");
+} else if (userPlay < pcPlay) {
+    console.log("il pc ha vinto");
+} else {
+    console.log("pareggio");
+}
+
+
 
 //Stabilire il vincitore
   //in base a chi fa il punteggio più alto
